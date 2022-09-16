9dc8e59f2fa784e55c8229c3df9459bed06201b9
Wickus Van der Merwe <vandermerwewickus@gmail.com>
2022-09-16 21:54:47 +0200

    :bug: Fixed file not found

--- pages/api/update-score.ts
+++ pages/api/update-score.ts
@@ -2,7 +2,7 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import fs from "fs";
 import axios from "axios";
-import {course} from "../../lib/config";
+import path from "path"
 
 export default function handler(
     req: NextApiRequest,
@@ -10,13 +10,13 @@ export default function handler(
 ) {
     try {
         axios.get("https://main.d22uf180o8wjsx.amplifyapp.com/round-1.json").then((courseData) => {
-            console.log(courseData)
+            const roundPath = path.join(process.cwd(), 'public');
             let data: { [key: string]: { stroke: number, par: number, strokes: number } } = JSON.parse(JSON.stringify(courseData.data))
             const request: { hole: number, score: number } = req.body
 
             data[request.hole.toString()].strokes = request.score
 
-            fs.writeFileSync("public/round-1.json", JSON.stringify(data))
+            fs.writeFileSync(roundPath + "/round-1.json", JSON.stringify(data))
 
             res.status(200).json("request")
 
