06a1faae4c9e4ad127c9348b7b8585a21dc5e755
Wickus Van der Merwe <vandermerwewickus@gmail.com>
2022-09-17 06:41:00 +0200

    :bug: Fixed file not found

--- pages/api/update-score.ts
+++ pages/api/update-score.ts
@@ -10,13 +10,13 @@ export default function handler(
 ) {
     try {
         axios.get("https://main.d22uf180o8wjsx.amplifyapp.com/round-1.json").then((courseData) => {
-            const roundPath = path.join(process.cwd(), 'public');
+            const roundPath = path.join(process.cwd(),"round-1.json");
             let data: { [key: string]: { stroke: number, par: number, strokes: number } } = JSON.parse(JSON.stringify(courseData.data))
             const request: { hole: number, score: number } = req.body
 
             data[request.hole.toString()].strokes = request.score
 
-            fs.writeFileSync(roundPath + "/round-1.json", JSON.stringify(data))
+            fs.writeFileSync(roundPath, JSON.stringify(data))
 
             res.status(200).json("request")
 
