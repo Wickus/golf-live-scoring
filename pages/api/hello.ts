4cfbbbec7e1e3710561dae294d429b820daedd78
Wickus Van der Merwe <vandermerwewickus@gmail.com>
2022-09-19 09:09:57 +0200

    :bug: added api data

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {readdirSync} from "fs";
 
 type Data = {
   name: string
+  root: string[]
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  const root = readdirSync(process.cwd())
+
+  res.status(200).json({ name: 'John Doe', root: root })
 }
